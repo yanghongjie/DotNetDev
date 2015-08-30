68be25498354bf8ff291a972dc02a50ebe9b5bdd
JohnYang <idotnet@outlook.com>
2015-08-30 18:41:30 +0800

    delete some file.

---
+++ src/Dev/Data/ObjectCloneHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev.Data
+{
+    /// <summary>
+    /// 对象克隆
+    /// </summary>
+    public class ObjectCloneHelper
+    {
+        /// <summary>
+        /// 克隆对象
+        /// </summary>
+        /// <typeparam name="T">对象的类型</typeparam>
+        /// <param name="RealObject">克隆的对象</param>
+        /// <returns>返回克隆的对象</returns>
+        public static T Clone<T>(T RealObject)
+        {
+            using (Stream objectStream = new MemoryStream())
+            {
+                IFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(objectStream, RealObject);
+                objectStream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(objectStream);
+            }
+        }
+    }
+}
---
+++ src/Dev/DataAnnotations/MinAttribute.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev.DataAnnotations
+{
+    /// <summary>
+    ///     最小值
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class MinAttribute : ValidationAttribute
+    {
+        /// <summary>
+        ///     最小值
+        /// </summary>
+        public decimal MinValue { get; set; }
+
+        /// <summary>
+        ///     构造函数
+        /// </summary>
+        /// <param name="minimun">最小值</param>
+        public MinAttribute(object minimun)
+        {
+            MinValue = Convert.ToDecimal(minimun);
+        }
+
+        /// <summary>
+        ///     验证
+        /// </summary>
+        /// <param name="value">验证值</param>
+        /// <returns>验证通过与否</returns>
+        public override bool IsValid(object value)
+        {
+            decimal thisValue;
+            if (value != null && decimal.TryParse(value.ToString(), out thisValue))
+            {
+                return (thisValue >= MinValue);
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///     错误提示
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format("{0} 最小值为 {1}", name, MinValue);
+        }
+    }
+}
